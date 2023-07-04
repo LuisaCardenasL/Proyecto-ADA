f6ec7cafb914371fcc97ff1210d5dc48eebb97dd
LuisaCardenasL <luisa072697@gmail.com>
2023-07-04 10:48:44 -0500

    al menos da resultado

--- O(n).py
+++ O(n).py
@@ -18,6 +18,12 @@ class Escena:
             duracion_total += parte.duracion
         return duracion_total
 
+    def obtener_grandeza_total(self):
+        grandeza_total = 0
+        for parte in self.partes:
+            grandeza_total += len(parte.nombre.split(", "))
+        return grandeza_total
+
 
 class Espectaculo:
     def __init__(self, nombre):
@@ -33,6 +39,47 @@ class Espectaculo:
             duracion_total += escena.obtener_duracion_total()
         return duracion_total
 
+    def obtener_animal_mas_participante(self):
+        participantes = {}
+        for escena in self.escenas:
+            for parte in escena.partes:
+                for animal in parte.nombre.split(", "):
+                    if animal in participantes:
+                        participantes[animal] += 1
+                    else:
+                        participantes[animal] = 1
+        animal_mas_participante = max(participantes, key=participantes.get)
+        return animal_mas_participante, participantes[animal_mas_participante]
+
+    def obtener_animales_menos_participantes(self):
+        participantes = {}
+        for escena in self.escenas:
+            for parte in escena.partes:
+                for animal in parte.nombre.split(", "):
+                    if animal in participantes:
+                        participantes[animal] += 1
+                    else:
+                        participantes[animal] = 1
+        min_participaciones = min(participantes.values())
+        animales_menos_participantes = [animal for animal, participaciones in participantes.items() if participaciones == min_participaciones]
+        return animales_menos_participantes, min_participaciones
+
+    def obtener_escena_menor_grandeza_total(self):
+        escena_menor_grandeza_total = min(self.escenas, key=lambda x: x.obtener_grandeza_total())
+        return escena_menor_grandeza_total.nombre
+
+    def obtener_escena_mayor_grandeza_total(self):
+        escena_mayor_grandeza_total = max(self.escenas, key=lambda x: x.obtener_grandeza_total())
+        return escena_mayor_grandeza_total.nombre
+
+    def obtener_promedio_grandeza_total(self):
+        total_grandeza = 0
+        total_escenas = len(self.escenas)
+        for escena in self.escenas:
+            total_grandeza += escena.obtener_grandeza_total()
+        promedio_grandeza = total_grandeza / total_escenas
+        return round(promedio_grandeza, 2)
+
 
 # Ejemplo de uso
 espectaculo = Espectaculo("Espectáculo de Circo")
@@ -40,8 +87,8 @@ espectaculo = Espectaculo("Espectáculo de Circo")
 # Crear escena 1
 escena1 = Escena("Escena 1")
 
-parte1 = Parte("Parte 1", 10)
-parte2 = Parte("Parte 2", 15)
+parte1 = Parte("Capibara, Loro, Caimán", 10)
+parte2 = Parte("Capibara, Caimán, Boa", 15)
 
 escena1.agregar_parte(parte1)
 escena1.agregar_parte(parte2)
@@ -49,16 +96,219 @@ escena1.agregar_parte(parte2)
 # Crear escena 2
 escena2 = Escena("Escena 2")
 
-parte3 = Parte("Parte 3", 12)
-parte4 = Parte("Parte 4", 8)
+parte3 = Parte("Capibara, Loro, Cocodrilo", 12)
+parte4 = Parte("Loro, Cocodrilo, Pantera negra", 8)
 
 escena2.agregar_parte(parte3)
 escena2.agregar_parte(parte4)
 
+# Crear escena 3
+escena3 = Escena("Escena 3")
+
+parte5 = Parte("Capibara, Loro, Tigre", 10)
+parte6 = Parte("Loro, Caimán, León", 15)
+
+escena3.agregar_parte(parte5)
+escena3.agregar_parte(parte6)
+
+# Crear escena 4
+escena4 = Escena("Escena 4")
+
+parte7 = Parte("Boa, Cocodrilo, León", 10)
+parte8 = Parte("Cebra, Pantera negra, Tigre", 15)
+
+escena4.agregar_parte(parte7)
+escena4.agregar_parte(parte8)
+
+# Crear escena 5
+escena5 = Escena("Escena 5")
+
+parte9 = Parte("Cebra, Pantera negra, León", 12)
+
+escena5.agregar_parte(parte9)
+
 # Agregar escenas al espectáculo
 espectaculo.agregar_escena(escena1)
 espectaculo.agregar_escena(escena2)
+espectaculo.agregar_escena(escena3)
+espectaculo.agregar_escena(escena4)
+espectaculo.agregar_escena(escena5)
 
 # Obtener duración total del espectáculo
 duracion_total = espectaculo.obtener_duracion_total()
 print("Duración total del espectáculo:", duracion_total)
+
+# Obtener participación de animales
+animal_mas_participante, cantidad_participaciones = espectaculo.obtener_animal_mas_participante()
+print("El animal que participó en más escenas dentro del espectáculo fue el", animal_mas_participante, "que participó en", cantidad_participaciones, "escenas.")
+
+animales_menos_participantes, min_participaciones = espectaculo.obtener_animales_menos_participantes()
+print("Los animales que menos participaron en escenas dentro del espectáculo fueron", ", ".join(animales_menos_participantes), "quienes participaron cada uno en", min_participaciones, "escenas.")
+
+# Obtener escenas con mayor y menor grandeza total
+escena_menor_grandeza_total = espectaculo.obtener_escena_menor_grandeza_total()
+print("La escena de menor grandeza total fue la escena", escena_menor_grandeza_total)
+
+escena_mayor_grandeza_total = espectaculo.obtener_escena_mayor_grandeza_total()
+print("La escena de mayor grandeza total fue la escena", escena_mayor_grandeza_total)
+
+# Obtener promedio de grandeza total
+promedio_grandeza = espectaculo.obtener_promedio_grandeza_total()
+print("El promedio de grandeza de todo el espectáculo fue de", promedio_grandeza)
+class Parte:
+    def __init__(self, nombre, duracion):
+        self.nombre = nombre
+        self.duracion = duracion
+
+
+class Escena:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.partes = []
+
+    def agregar_parte(self, parte):
+        self.partes.append(parte)
+
+    def obtener_duracion_total(self):
+        duracion_total = 0
+        for parte in self.partes:
+            duracion_total += parte.duracion
+        return duracion_total
+
+    def obtener_grandeza_total(self):
+        grandeza_total = 0
+        for parte in self.partes:
+            grandeza_total += len(parte.nombre.split(", "))
+        return grandeza_total
+
+
+class Espectaculo:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.escenas = []
+
+    def agregar_escena(self, escena):
+        self.escenas.append(escena)
+
+    def obtener_duracion_total(self):
+        duracion_total = 0
+        for escena in self.escenas:
+            duracion_total += escena.obtener_duracion_total()
+        return duracion_total
+
+    def obtener_animal_mas_participante(self):
+        participantes = {}
+        for escena in self.escenas:
+            for parte in escena.partes:
+                for animal in parte.nombre.split(", "):
+                    if animal in participantes:
+                        participantes[animal] += 1
+                    else:
+                        participantes[animal] = 1
+        animal_mas_participante = max(participantes, key=participantes.get)
+        return animal_mas_participante, participantes[animal_mas_participante]
+
+    def obtener_animales_menos_participantes(self):
+        participantes = {}
+        for escena in self.escenas:
+            for parte in escena.partes:
+                for animal in parte.nombre.split(", "):
+                    if animal in participantes:
+                        participantes[animal] += 1
+                    else:
+                        participantes[animal] = 1
+        min_participaciones = min(participantes.values())
+        animales_menos_participantes = [animal for animal, participaciones in participantes.items() if participaciones == min_participaciones]
+        return animales_menos_participantes, min_participaciones
+
+    def obtener_escena_menor_grandeza_total(self):
+        escena_menor_grandeza_total = min(self.escenas, key=lambda x: x.obtener_grandeza_total())
+        return escena_menor_grandeza_total.nombre
+
+    def obtener_escena_mayor_grandeza_total(self):
+        escena_mayor_grandeza_total = max(self.escenas, key=lambda x: x.obtener_grandeza_total())
+        return escena_mayor_grandeza_total.nombre
+
+    def obtener_promedio_grandeza_total(self):
+        total_grandeza = 0
+        total_escenas = len(self.escenas)
+        for escena in self.escenas:
+            total_grandeza += escena.obtener_grandeza_total()
+        promedio_grandeza = total_grandeza / total_escenas
+        return round(promedio_grandeza, 2)
+
+
+# Ejemplo de uso
+espectaculo = Espectaculo("Espectáculo de Circo")
+
+# Crear escena 1
+escena1 = Escena("Escena 1")
+
+parte1 = Parte("Capibara, Loro, Caimán", 10)
+parte2 = Parte("Capibara, Caimán, Boa", 15)
+
+escena1.agregar_parte(parte1)
+escena1.agregar_parte(parte2)
+
+# Crear escena 2
+escena2 = Escena("Escena 2")
+
+parte3 = Parte("Capibara, Loro, Cocodrilo", 12)
+parte4 = Parte("Loro, Cocodrilo, Pantera negra", 8)
+
+escena2.agregar_parte(parte3)
+escena2.agregar_parte(parte4)
+
+# Crear escena 3
+escena3 = Escena("Escena 3")
+
+parte5 = Parte("Capibara, Loro, Tigre", 10)
+parte6 = Parte("Loro, Caimán, León", 15)
+
+escena3.agregar_parte(parte5)
+escena3.agregar_parte(parte6)
+
+# Crear escena 4
+escena4 = Escena("Escena 4")
+
+parte7 = Parte("Boa, Cocodrilo, León", 10)
+parte8 = Parte("Cebra, Pantera negra, Tigre", 15)
+
+escena4.agregar_parte(parte7)
+escena4.agregar_parte(parte8)
+
+# Crear escena 5
+escena5 = Escena("Escena 5")
+
+parte9 = Parte("Cebra, Pantera negra, León", 12)
+
+escena5.agregar_parte(parte9)
+
+# Agregar escenas al espectáculo
+espectaculo.agregar_escena(escena1)
+espectaculo.agregar_escena(escena2)
+espectaculo.agregar_escena(escena3)
+espectaculo.agregar_escena(escena4)
+espectaculo.agregar_escena(escena5)
+
+# Obtener duración total del espectáculo
+duracion_total = espectaculo.obtener_duracion_total()
+print("Duración total del espectáculo:", duracion_total)
+
+# Obtener participación de animales
+animal_mas_participante, cantidad_participaciones = espectaculo.obtener_animal_mas_participante()
+print("El animal que participó en más escenas dentro del espectáculo fue el", animal_mas_participante, "que participó en", cantidad_participaciones, "escenas.")
+
+animales_menos_participantes, min_participaciones = espectaculo.obtener_animales_menos_participantes()
+print("Los animales que menos participaron en escenas dentro del espectáculo fueron", ", ".join(animales_menos_participantes), "quienes participaron cada uno en", min_participaciones, "escenas.")
+
+# Obtener escenas con mayor y menor grandeza total
+escena_menor_grandeza_total = espectaculo.obtener_escena_menor_grandeza_total()
+print("La escena de menor grandeza total fue la escena", escena_menor_grandeza_total)
+
+escena_mayor_grandeza_total = espectaculo.obtener_escena_mayor_grandeza_total()
+print("La escena de mayor grandeza total fue la escena", escena_mayor_grandeza_total)
+
+# Obtener promedio de grandeza total
+promedio_grandeza = espectaculo.obtener_promedio_grandeza_total()
+print("El promedio de grandeza de todo el espectáculo fue de", promedio_grandeza)
--- O(n2).py
+++ O(n2).py
@@ -1,68 +1,75 @@
-def organizar_espectaculo(escenas):
-    # Ordenar las escenas de cada parte
-    for parte in escenas:
-        bubble_sort(parte, key=lambda escena: escena_grandeza_total(escena))
+def zoo_show(n, m, k, animals, scenes):
+    apertura = []
+    partes = []
 
-    # Ordenar las partes en el espectáculo principal
-    bubble_sort(escenas, key=lambda parte: parte_grandeza_total(parte))
+    for i in range(m):
+        apertura.append(scenes[i])
 
-    # Obtener información adicional
-    animal_mas_participacion, animal_menos_participacion = obtener_animal_participacion(
-        escenas)
-    escena_menor_grandeza, escena_mayor_grandeza = obtener_escenas_grandeza(
-        escenas)
-    promedio_grandeza = calcular_promedio_grandeza(escenas)
+    for i in range(m, m + k):
+        partes.append(scenes[i])
 
-    # Retornar el resultado
-    return {
-        'escenas_ordenadas': escenas,
-        'animal_mas_participacion': animal_mas_participacion,
-        'animal_menos_participacion': animal_menos_participacion,
-        'escena_menor_grandeza': escena_menor_grandeza,
-        'escena_mayor_grandeza': escena_mayor_grandeza,
-        'promedio_grandeza': promedio_grandeza
-    }
+    animal_counts = {}
+    total_grandeur = 0
 
+    for animal in animals:
+        animal_counts[animal[0]] = 0
 
-def bubble_sort(array, key):
-    n = len(array)
-    for i in range(n - 1):
-        for j in range(n - 1 - i):
-            if key(array[j]) > key(array[j + 1]):
-                array[j], array[j + 1] = array[j + 1], array[j]
+    for scene in apertura:
+        total_grandeur += sum([animal[1] for animal in scene])
+        for animal in scene:
+            animal_counts[animal[0]] += 1
 
+    for part in partes:
+        total_grandeur += sum([animal[1] for animal in part])
 
-def escena_grandeza_total(escena):
-    return sum(animal.grandeza for animal in escena)
+    average_grandeur = total_grandeur / (len(apertura) + len(partes))
 
+    max_participation = max(animal_counts.values())
+    min_participation = min(animal_counts.values())
 
-def parte_grandeza_total(parte):
-    return sum(escena_grandeza_total(escena) for escena in parte)
+    most_participated_animals = [animal for animal, count in animal_counts.items() if count == max_participation]
+    least_participated_animals = [animal for animal, count in animal_counts.items() if count == min_participation]
 
+    scenes_apertura = ", ".join([str(scene) for scene in apertura])
+    scenes_partes = [", ".join([str(scene) for scene in part]) for part in partes]
 
-def obtener_animal_participacion(escenas):
-    conteo_animal = {}
-    for parte in escenas:
-        for escena in parte:
-            for animal in escena:
-                conteo_animal[animal] = conteo_animal.get(animal, 0) + 1
+    print("Apertura: ", scenes_apertura)
+    print("Parte 1: ", scenes_partes[0])
+    print("Parte 2: ", scenes_partes[1])
+    print("Parte 3: ", scenes_partes[2])
+    print("El animal que participó en más escenas dentro del espectáculo fue: ", most_participated_animals, "que participó en", max_participation, "escenas.")
+    print("El animal que menos participó en escenas dentro del espectáculo fueron: ", least_participated_animals, "que participó en", min_participation, "escenas.")
+    print("La escena de menor grandeza total fue la escena", min(apertura + partes, key=lambda x: sum([animal[1] for animal in x])))
+    print("La escena de mayor grandeza total fue la escena", max(apertura + partes, key=lambda x: sum([animal[1] for animal in x])))
+    print("El promedio de grandeza de todo el espectáculo fue de", round(average_grandeur, 2))
 
-    animal_mas_participacion = max(conteo_animal, key=conteo_animal.get)
-    animal_menos_participacion = min(conteo_animal, key=conteo_animal.get)
+# Ejemplo de uso
+n = 9
+m = 4
+k = 3
 
-    return animal_mas_participacion, animal_menos_participacion
+animals = [
+    ("Capibara", 1),
+    ("Loro", 2),
+    ("Caiman", 3),
+    ("Boa", 4),
+    ("Cocodrilo", 5),
+    ("Cebra", 6),
+    ("Pantera negra", 7),
+    ("Tigre", 8),
+    ("León", 9)
+]
 
+scenes = [
+    [("Caiman", 3), ("Capibara", 1), ("Loro", 2)],
+    [("Boa", 4), ("Caiman", 3), ("Capibara", 1)],
+    [("Cocodrilo", 5), ("Capibara", 1), ("Loro", 2)],
+    [("Pantera negra", 7), ("Cocodrilo", 5), ("Loro", 2)],
+    [("Tigre", 8), ("Loro", 2), ("Capibara", 1)],
+    [("Leon", 9), ("Caiman", 3), ("Loro", 2)],
+    [("León", 9), ("Cocodrilo", 5), ("Boa", 4)],
+    [("León", 9), ("Pantera negra", 7), ("Cebra", 6)],
+    [("Tigre", 8), ("Cebra", 6), ("Pantera negra", 7)]
+]
 
-def obtener_escenas_grandeza(escenas):
-    escena_menor_grandeza = min(escenas, key=escena_grandeza_total)
-    escena_mayor_grandeza = max(escenas, key=escena_grandeza_total)
-
-    return escena_menor_grandeza, escena_mayor_grandeza
-
-
-def calcular_promedio_grandeza(escenas):
-    total_grandezas = sum(escena_grandeza_total(escena)
-                          for parte in escenas for escena in parte)
-    total_escenas = sum(len(parte) for parte in escenas)
-
-    return total_grandezas / total_escenas
+zoo_show(n, m, k, animals, scenes)
